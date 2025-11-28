e05dc795dd6a44532022d7040992059095878a61
19tracks <19tracks@tuta.io>
2025-11-27 20:55:16 -0800

    graft in BLAKE3's key schedule and number of rounds

--- source/common/wifi_rand/wifi_rand_core.c
+++ source/common/wifi_rand/wifi_rand_core.c
@@ -11,18 +11,9 @@ static const uint32_t Wifi_Rand_Core_Iv[8] =
   0x510E527FUL, 0x9B05688CUL, 0x1F83D9ABUL, 0x5BE0CD19UL
 };
 
-static const uint8_t Wifi_Rand_Core_Sigma[10][16] =
+static const uint8_t Wifi_Rand_Core_Sigma[16] =
 {
-  {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 } ,
-  { 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 } ,
-  { 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 } ,
-  {  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 } ,
-  {  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 } ,
-  {  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 } ,
-  { 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 } ,
-  { 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 } ,
-  {  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 } ,
-  { 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13 , 0 } ,
+  2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8
 };
 
 static void Wifi_Rand_Core_SetLastnode( Wifi_Rand_Core_State *S )
@@ -130,35 +121,36 @@ int Wifi_Rand_Core_InitKey( Wifi_Rand_Core_State *S, size_t outlen, const void *
   return 0;
 }
 
-#define G(r,i,a,b,c,d)                      \
+#define G(sigma,i,a,b,c,d)                      \
   do {                                      \
-    a = a + b + m[Wifi_Rand_Core_Sigma[r][2*i+0]]; \
+    a = a + b + m[sigma[2*i+0]]; \
     d = rotr32(d ^ a, 16);                  \
     c = c + d;                              \
     b = rotr32(b ^ c, 12);                  \
-    a = a + b + m[Wifi_Rand_Core_Sigma[r][2*i+1]]; \
+    a = a + b + m[sigma[2*i+1]]; \
     d = rotr32(d ^ a, 8);                   \
     c = c + d;                              \
     b = rotr32(b ^ c, 7);                   \
   } while(0)
 
-#define ROUND(r)                    \
+#define ROUND(sigma)                    \
   do {                              \
-    G(r,0,v[ 0],v[ 4],v[ 8],v[12]); \
-    G(r,1,v[ 1],v[ 5],v[ 9],v[13]); \
-    G(r,2,v[ 2],v[ 6],v[10],v[14]); \
-    G(r,3,v[ 3],v[ 7],v[11],v[15]); \
-    G(r,4,v[ 0],v[ 5],v[10],v[15]); \
-    G(r,5,v[ 1],v[ 6],v[11],v[12]); \
-    G(r,6,v[ 2],v[ 7],v[ 8],v[13]); \
-    G(r,7,v[ 3],v[ 4],v[ 9],v[14]); \
+    G(sigma,0,v[ 0],v[ 4],v[ 8],v[12]); \
+    G(sigma,1,v[ 1],v[ 5],v[ 9],v[13]); \
+    G(sigma,2,v[ 2],v[ 6],v[10],v[14]); \
+    G(sigma,3,v[ 3],v[ 7],v[11],v[15]); \
+    G(sigma,4,v[ 0],v[ 5],v[10],v[15]); \
+    G(sigma,5,v[ 1],v[ 6],v[11],v[12]); \
+    G(sigma,6,v[ 2],v[ 7],v[ 8],v[13]); \
+    G(sigma,7,v[ 3],v[ 4],v[ 9],v[14]); \
   } while(0)
 
 static void Wifi_Rand_Core_Compress( Wifi_Rand_Core_State *S, const uint8_t in[WIFI_RAND_CORE_BLOCKBYTES] )
 {
   uint32_t m[16];
   uint32_t v[16];
-  size_t i;
+  uint8_t sigma_1[16], sigma_2[16];
+  size_t i, j;
 
   for( i = 0; i < 16; ++i ) {
     m[i] = load32( in + i * sizeof( m[i] ) );
@@ -177,16 +169,28 @@ static void Wifi_Rand_Core_Compress( Wifi_Rand_Core_State *S, const uint8_t in[W
   v[14] = S->f[0] ^ Wifi_Rand_Core_Iv[6];
   v[15] = S->f[1] ^ Wifi_Rand_Core_Iv[7];
 
-  ROUND( 0 );
-  ROUND( 1 );
-  ROUND( 2 );
-  ROUND( 3 );
-  ROUND( 4 );
-  ROUND( 5 );
-  ROUND( 6 );
-  ROUND( 7 );
-  ROUND( 8 );
-  ROUND( 9 );
+  for( i = 0; i < 16; ++i) {
+    sigma_1[i] = i;
+  }
+
+  for( i = 0; ; ) {
+    ROUND( sigma_1 );
+
+    // 7 rounds; check here because the number is odd.
+    if( i + 1 >= 7 ) break;
+
+    for( j = 0; j < 16; ++j ) {
+      sigma_2[j] = sigma_1[Wifi_Rand_Core_Sigma[j]];
+    }
+
+    ROUND( sigma_2 );
+
+    for( j = 0; j < 16; ++j ) {
+      sigma_1[j] = sigma_2[Wifi_Rand_Core_Sigma[j]];
+    }
+
+    i += 2;
+  }
 
   for( i = 0; i < 8; ++i ) {
     S->h[i] = S->h[i] ^ v[i] ^ v[i + 8];
