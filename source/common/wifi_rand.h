bd3a825bbbcc49ce9da006ee00669993df43b177
19tracks <19tracks@tuta.io>
2025-12-01 14:54:26 -0800

    comment functions in wifi_rand.h

--- source/common/wifi_rand.h
+++ source/common/wifi_rand.h
@@ -39,15 +39,49 @@ typedef struct Wifi_Rand_FinishedState__
     uint32_t inputCounter;
 } Wifi_Rand_FinishedState;
 
+// Initialize a hasher. No entropy is sampled; the state the hasher is left in
+// after this function is called is always the same.
 void Wifi_Rand_Init(Wifi_Rand_State *state);
+// Check whether any expensive compression steps will be triggered by a call to
+// Wifi_Rand_Update with the given inlen. If this returns false, such a call to
+// Wifi_Rand_Update will only copy inlen bytes into its internal buffer and
+// update bookkeeping.
 bool Wifi_Rand_WillUpdateTriggerCompress(Wifi_Rand_State *state, size_t inlen);
+// Add the given bytes to state. If the internal buffer would overflow, the
+// compression function is called as many times as necessary to absorb the
+// entropy from every byte.
 void Wifi_Rand_Update(Wifi_Rand_State *state, const void *in, size_t inlen);
+// Create a Wifi_Rand_FinishedState from the current state. state is unchanged,
+// and finished is freshly initialized. This runs the compression function.
 void Wifi_Rand_Finish(Wifi_Rand_State *state, Wifi_Rand_FinishedState *finished);
 
+// Check whether any expensive compression steps will be triggered by a call to
+// Wifi_Rand_FinishedReadBytes with the given outlen. If this returns false,
+// such a call to Wifi_Rand_FinishedReadBytes will only copy outlen bytes from
+// its internal buffer and update bookkeeping.
 bool Wifi_Rand_WillFinishedReadTriggerCompress(Wifi_Rand_FinishedState *state, size_t outlen);
+// Read outlen pseudorandom bytes. The stream of bytes given as output will be
+// unique to the stream of bytes that were given as input to the Wifi_Rand_State
+// that was used to create this Wifi_Rand_FinishedState, and a given
+// Wifi_Rand_FinishedState will never produce the same sequence of bytes
+// multiple times except by chance or after producing 128 GiB (at which point
+// the output loops). If the request can't be fulfilled from the internal
+// buffer, the compression function is called as many times as necessary to
+// fulfill it.
 void Wifi_Rand_FinishedReadBytes(Wifi_Rand_FinishedState *finished, void *out, size_t outlen);
 
+// Initialize `reservation` as a fork of `finished` and advance the byte stream
+// `finished` far enough to make sure `finished` and `reservation` don't produce
+// any of the same bytes unless by chance. Do not use `reservation` to read more
+// than `outlen` bytes before discarding it, or this guarantee may not hold.
+// Avoid passing excessively large values of `outlen` as well; the limit of
+// 128 GiB of output before a Wifi_Rand_FinishedState loops is normally an
+// astronomically large number of bytes for a DS, but hitting that limit becomes
+// much more feasible if this function is abused.
 void Wifi_Rand_FinishedReserveBytes(Wifi_Rand_FinishedState *finished, Wifi_Rand_FinishedState *reservation, size_t outlen);
+// Add bytes from the internal buffer of `reservation` to the internal buffer of
+// `finished`. Call this before discarding `reservation` so the processing time
+// spent generating those bytes isn't wasted.
 void Wifi_Rand_FinishedEndReservation(Wifi_Rand_FinishedState *finished, Wifi_Rand_FinishedState *reservation);
 
 #endif
