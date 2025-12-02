997338062d3a82c57a4fb31e3e8ae555e9ea3acd
19tracks <19tracks@tuta.io>
2025-12-01 17:04:57 -0800

    cast to the actual type instead of void* to strip volatile

--- source/arm9/ipc.c
+++ source/arm9/ipc.c
@@ -90,7 +90,7 @@ static bool Wifi_InitIPC(unsigned int flags)
     for (u8 i = 0; i < PersonalData->nameLen; i++)
         WifiData->hostPlayerName[i] = PersonalData->name[i];
 
-    Wifi_Rand_InitHasher((void*)&WifiData->entropyHasher.hasher);
+    Wifi_Rand_InitHasher((Wifi_Rand_Hasher*)&WifiData->entropyHasher.hasher);
     // The spinlock was initialized to 0 with memset.
     // So were the RNG hashers' total_input_bytes.
     // Now since we wrote to entropyHasher.hasher with its
